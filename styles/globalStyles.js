823182e19ba8b6954cecd579c31590577118f44e
Volker Bransky <vBransky@googlemail.com>
2021-12-15 19:00:26 +0100

    change: globalStyles var and body

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -2,13 +2,24 @@ import { createGlobalStyle } from "styled-components";
 
 const globalStyles = createGlobalStyle`
 :root {
-    --color-bg-main = rgb(155 155 155 / 30%)
+--color-bg-main: rgb(0 0 0 / 90%);
 }
 
+html,
 body {
-    box-sizing: border-box;
-    margin:0;
-    padding:0; 
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
 }
 `;
 
